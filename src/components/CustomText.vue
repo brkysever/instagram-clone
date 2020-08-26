59d2064d3f7dfb4ac8d68e1a7327c19cbf950f55
brkysever <brkysever@gmail.com>
2020-08-26 23:55:29 +0300

    Text

---
+++ src/components/CustomText.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'Text',
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    size: {
+      type: String,
+      default: 'normal',
+      validator: function(value) {
+        return ['normal', 'small', 'xsmall', 'xxsmall'].indexOf(value) !== -1
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.small {
+  font-size: 12px;
+}
+.xsmall {
+  font-size: 11px;
+}
+.xxsmall {
+  font-size: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText>Merhaba Dünya</CustomText>
+    <CustomText tag="p">Merhaba Dünya</CustomText>
+    <CustomText tag="b" size="xsmall">Merhaba Dünya</CustomText>
   </div>
 </template>
 
 <script>
+import CustomText from '@/components/CustomText'
+
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  component: {
+    CustomText
+  }
 }
 </script>
 
